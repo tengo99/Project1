b4131124e30f36c5c2fbafdb09b7b8e2fcc7baae
ChoiKiWon <longvaca0213@gmail.com>
2024-01-25 17:34:28 +0900

    세번째 커밋 제곱함수

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ def add_func(n1, n2):
 def sub_func(n1,n2):
     return n1-n2
 
+def sub2_func(n1):
+    return n1*n1
+
 ##전역 변수부
 num1, num2, res = 100, 200, 0
  
@@ -15,3 +18,8 @@ print(num1, '+', num2, '=', res)
 
 res = sub_func(num1, num2)
 print(num1, '-', num2, '=', res)
+
+res = sub2_func(num1)
+print(num1, '*', num1, '=', res)
+
+
